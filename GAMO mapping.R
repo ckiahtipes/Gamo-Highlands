7066473e45c01cc61ba23534fc8f38e06a887dff
Chris Kiahtipes <greeksciencecorp@Chriss-Mac-Studio.local>
2026-04-26 20:12:34 -0400

    Staging vegetation cover mapping.

--- GAMO mapping.R
+++ GAMO mapping.R
@@ -6,6 +6,7 @@ library(geodata)
 library(maps)
 library(grDevices)
 library(sf)
+library(raster)
 library(rnaturalearth)
 
 #Cores
@@ -34,8 +35,28 @@ if(!file.exists("SRTM/elevation/srtm_44_11.tif")){
   GAMO_dem <- rast(GAMO_pth) 
 }
 
+#Let's use the country map to extract data more precisely.
+
+ETHP_box = st_bbox(ETHP_dem)
+
+#Let's pull some of the landcover data
+
+ETHP_tree = landcover("trees", path = "LAND/")
+ETHP_grss = landcover("grassland", path = "LAND/")
+ETHP_crop = landcover("cropland", path = "LAND/")
+
+ETHP_tree = crop(ETHP_tree, ETHP_dem)
+
+ESAtrsh_col=c(NA,"#6c9575")
+ESAgrcp_col=c(NA,"#ffd731")
+ESAcrop_col=c(NA,"darkred")
+
 par(mfrow = c(1,2))
 
+plot(0, xlim = c(32, 48), ylim = c(3, 15), pch = NA, axes = FALSE, ann = FALSE)
+plot(ETHP_tree, add = TRUE, alpha = 0.8, col = ESAtrsh_col, legend = FALSE)
+plot(ETHP_grss, add = TRUE, alpha = 0.8, col = ESAgrcp_col, legend = FALSE)
+plot(ETHP_crop, add = TRUE, alpha = 0.8, col = ESAcrop_col, legend = FALSE)
 plot(ETHP_dem, col = gray.colors(4661), main = "Ethiopia")
 
 polygon(c(lon,rev(lon)),c(rep(lat, each = 2)), col = NA, lwd = 2, border = "forestgreen")
@@ -44,4 +65,4 @@ plot(GAMO_dem, xlim = lon, ylim = lat, col = gray.colors(3550), main = "Gamo Hig
 
 points(clong, clats, pch = 21, bg = "goldenrod")
 
-par(mfrow = c(2,1))
+par(mfrow = c(1,1))
